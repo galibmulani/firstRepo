2c61664a4f48b8f1be4044dc5e1f37c4e93a26bc
Galib Mulani <46106390+galibmulani@users.noreply.github.com>
2023-08-25 02:06:27 +0530

    Update vector in cpp

--- vector.cpp
+++ vector.cpp
@@ -1,5 +1,13 @@
-#include<stdio>
-#include<conio>
+#include<iostream>
 #include<vector>
+
 using namespace std;
 
+int main(){
+vector<int> vector1 = {1, 2, 3, 4, 5};
+
+for (const int &i : vector){
+  cout<<i;
+}
+return 0;
+}
